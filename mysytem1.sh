4a141d873a88890cb71602cb6104efb3e136cec5
HerbertVaughn <consciousdesigns3@gmail.com>
2020-12-03 00:20:41 -0600

    Add files via upload

---
+++ mysytem1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "This is information by mysytem.sh. Program starts now."
+
+echo "Hello $USER"
+echo
+
+echo "Today's date is 'date', this is week 'date +"%v"."
+echo
+
+
+echo "Theses users are curently connected:"
