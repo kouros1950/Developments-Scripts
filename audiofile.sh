1f6a906fba84dda043e4753695175c0a7eae64bd
HerbertVaughn <consciousdesigns3@gmail.com>
2020-12-03 00:25:11 -0600

    Add files via upload

---
+++ audiofile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This is a simple script that will cause your system to play sound at the start and stopping of your system
+
+# This script is for /etc/rc.d/init.d
+# Link in rc3.d/S99audio-greeting and rc0.d/K01audio-greeting
+
+case "$1" in
+'start')
+  cat /usr/share/audio/at_your_service.au > /dev/audio
+  ;;
+'stop')
+  cat /usr/share/audio/oh_no_not_again.au > /dev/audio
+  ;;
+esac
+exit 0
+
+
